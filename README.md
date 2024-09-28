c4623438b0cbcf62dbc4f42521547a86fd935348
Bohdan Lupyr <70830768+Brolupyr777@users.noreply.github.com>
2024-09-28 15:50:12 -0700

    Website and Readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Metrohacks-oompa-loompa
+
+This is a website that estimates the Vancouver's population by using an AI Image Recognition Model that counts the amount of cars on the highways and calculates the approximate population based on the average amount of cars on the roads
---
+++ main.js
@@ -0,0 +1,9 @@
+// let map = L.map("map", {
+//   center: [51.505, -0.09],
+//   zoom: 13,
+// });
+// population / amount of cars (population estimate) = scaling factor
+// scaling factor * amount of cars (averaged out) = population estimate
+let cars = 5;
+let population = 200000;
+document.querySelector(".number").textContent = population;

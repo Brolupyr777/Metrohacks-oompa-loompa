c888e138cfbc95fae037955f037cb3ac49d017ec
frames per second man <artemisbeams@gmail.com>
2024-09-28 16:15:03 -0700

    updated js

--- main.js
+++ main.js
@@ -4,6 +4,14 @@
 // });
 // population / amount of cars (population estimate) = scaling factor
 // scaling factor * amount of cars (averaged out) = population estimate
-let cars = 5;
-let population = 200000;
-document.querySelector(".number").textContent = population;
+const scalingFactor = 3.5;
+const getData = async () => {
+  let url = "https://253c-34-125-46-57.ngrok-free.app/";
+  const response = await fetch(url);
+  console.log(response);
+  let cars = response;
+  let population = cars * scalingFactor;
+  document.querySelector(".number").textContent = population;
+}
+
+
